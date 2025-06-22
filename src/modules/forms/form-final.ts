6199a05ac815183a3493585ec10a2b66904ae48e
Alexander Berezhnev <freelancerbav@gmail.com>
2025-06-22 08:27:41 +0300

    v3.3.9 Added active links to Yandex Maps using the coordinates of the start location and weather.

--- src/modules/forms/form-final.ts
+++ src/modules/forms/form-final.ts
@@ -1,21 +1,32 @@
 import { errorHandler } from '../../errors/error.js';
 import { IPostData } from '../../interface/postdata.interdace.js';
+import { generateYandexMapLink } from '../../utils/telegram.js';
 import { getTimeLeft } from '../../utils/time-left.js';
 
 export function formFinalPost(postData: IPostData) {
   try {
+    const startCoords = postData.startLocation?.coords;
+    const weatherCoords = postData.weatherLocation?.coords;
+
+    const startYandexMapLink = startCoords ? generateYandexMapLink(...startCoords) : '';
+    const weatherYandexMapLink = weatherCoords ? generateYandexMapLink(...weatherCoords) : '';
+
     //строка о погоде
     const tempDay = postData.tempDay ? `${Math.round(+postData.tempDay)}°C` : '-';
     const humidity = `${postData.humidity ?? '-'}%`;
     const descriptionWeather = `${postData.descriptionWeather ?? '-'}`;
     const weatherStr = `${tempDay}, ${humidity}, ${descriptionWeather};`;
-    const weatherTitle = `<b>Погода (${postData.weatherLocation?.name ?? '---'}):</b>`;
+    const weatherTitle = `<b>Погода (<a href='${weatherYandexMapLink}'>${
+      postData.weatherLocation?.name ?? '---'
+    }</a>):</b>`;
 
     // время до старта
     const timeLeft = getTimeLeft(postData.dateStart, postData.time);
 
     const descriptionStr = `${postData.description ?? 'Детали заезда:'}\n`;
-    const locationStr = `<b>Место старта:</b> ${postData.startLocation?.name ?? '---'};\n`;
+    const locationStr = `<b>Место старта:</b> <a href='${startYandexMapLink}'>${
+      postData.startLocation?.name ?? '---'
+    }</a>;\n`;
     const dateStartStr = `<b>Дата заезда:</b> ${postData.dateStart ?? '---'};\n`;
     const timeStr = `<b>Время старта:</b> ${postData.time ?? '---'};\n`;
     const timeLeftStr = `<b>Осталось до старта:</b> ${timeLeft ?? '-'};\n`;
